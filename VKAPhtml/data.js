5e3c4a8b2f4fd80a59612598b6cd8464940aa5da
Valtteri <valtteri.putaansuu@gmail.com>
2024-09-27 20:19:04 +0300

    fuck language switcher

--- VKAPhtml/data.js
+++ VKAPhtml/data.js
@@ -1,19 +1,9 @@
 const langIndex = {
     fin: {
-        h2: "Koti",
-        p1: "Tervetuloa kotisivuni... kotisivulle:)",
-        pre1: "Tällä hetkellä, täällä ei ole paljoa nähtävää:)",
-        pre2: "Todennäköisesti laitan jotain mielenkiintoista tänne:)",
-        h3: "Siistejä linkkejä ja sellasta:",
-        p2: "Lempi videopeli ost"
+        h2: "Terve"
     },
 
     en: {
-        h2: "Home",
-        p1: "This is the homepage of my ... homepage:)",
-        pre1: "Currently there is not a lot to see here:)",
-        pre2: "I'll probably put intresting stuff here:)",
-        h3: "Cool stuff n links:",
-        p2: "My favourite game ost"
+        h2: "Hello"
     }
 }
